59abd012229c3756658f4fc68db303b879662a9c
khanmdalam <nooralam1342004@gmail.com>
2026-03-30 01:06:45 +0530

    chnge

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
+
 
 dotenv.config();
 
@@ -28,6 +31,24 @@ try {
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+// code for deployment 
+// if (process.env.NODE_ENV === "production") {
+//   const dirPath = path.resolve();
+
+//   app.use(express.static("./FrontEnd/dist"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(dirPath, "./FrontEnd/dist", "index.html")
+//     );
+//   });
+// }
+
+
+
+
+
 
 server.listen(PORT, () => {
   console.log(`Server is Running on port ${PORT}`);
