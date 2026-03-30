e581550055fb3ef302322f0dcb74f9392a445f94
khanmdalam <nooralam1342004@gmail.com>
2026-03-30 23:06:33 +0530

    app compled

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import path from "path";
+
 
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -33,17 +33,17 @@ app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
 // code for deployment 
-// if (process.env.NODE_ENV === "production") {
-//   const dirPath = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  const dirPath = path.resolve();
 
-//   app.use(express.static("./FrontEnd/dist"));
+  app.use(express.static("./FrontEnd/dist"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(
-//       path.resolve(dirPath, "./FrontEnd/dist", "index.html")
-//     );
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(dirPath, "./FrontEnd/dist", "index.html")
+    );
+  });
+}
 
 
 
