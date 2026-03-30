12cf84b87b763b69c0b6d8540d3058ad978c3c98
khanmdalam <nooralam1342004@gmail.com>
2026-03-31 01:06:59 +0530

    url update

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://fullstack-chatapp-2-icla.onrender.com",
     methods: ["GET", "POST"],
   },
 });
